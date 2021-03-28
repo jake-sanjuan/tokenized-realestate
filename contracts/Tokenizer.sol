5adeec37c4183d3a5d691724542a128fa7fcf963
Jacob San Juan <contactjsj42@gmail.com>
2021-03-28 09:13:57 -0400

    updates

--- contracts/Tokenizer.sol
+++ contracts/Tokenizer.sol
@@ -8,39 +8,50 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Need burnable function to allow properties to be taken off-chain
 // Need oracle call to verify agent
 // Need to figure out IPFS storage
+// Need to verify propertyOwner
 
 /*
-  Need to figure out how to verify authenticity of agent.  Ideas:
+  Need to figure out how to verify authenticity of agent.  Ideas (Extra to Oracle):
     - KYC with license and real estate license
     - Have owner of properties validate
     - Have already approved agent validate data offchain after oracle call **
-    - Can also configure way to pull data from any public resource and
-      query via oracle.  Could use this to validate that person that owns property
-      to be tokenized actually owns it, and have that person validate agent.
 */
 
 /*
   Will proabably need to validate transactions that have happened as well.
-  A few ways to do this:
+  A few ways to do this (All extra to oracle):
     - Multiple approved real estate agents can validate onchain
-    - Can configure way to get data from any API, I know link does this but
-      it is moreso figuring out how to make it easy for the user / can we switch
-      out the api the oracle is calling and handle all data returned, endpoints
-      will likely differ
     - Escrow type transaction where both seller and buyer approve transaction
       proving that it happened
+    - Best idea is likley to have owner validation.  If owner is validated and
+      proved to be a person, there is no problem here
 */
+
+/*
+  Real estate stuff
+    - Need title, certificate of title, deed uploaded to IPFS
+*/
+
+// Property mint and transfer must be approved by owner
 
 contract Tokenizer is ERC721 {
 
-  // Enum forSale?
-
   mapping (address => bool) public licensed;
+  mapping (address => bool) public owner;
+  mapping (address => Property) public ownerToProperty;
+  mapping (Property => bool) public approvedProperties;
+
+  struct Property {
+    bytes32 propertyOwner; // Hash
+    bytes32 propertyAddress; // Hash
+    uint tokenId;
+    bool approved;  // Approved by owner?
+  }
 
   public string baseURI;
   private uint tokenId;
 
-  constructor() ERC20("Property Token", "PT") {
+  constructor() ERC721("Property Token", "PT") {
     tokenId = 0;
   }
 
@@ -48,13 +59,36 @@ contract Tokenizer is ERC721 {
     require(licensed[msg.sender] == true, "Must be verified to call function.");
   }
 
-  function mint(address to) public onlyLicensed {
-    _safeMint(to, tokenId);
+  modifier onlyPropertyOwner() {
+    require(owner[msg.sender] == true, "Must be property owner to call function.");
+  }
+
+  // Pass struct here
+  modifier onlyApproved() {
+    require(approvedProperties[])
+  }
+
+  function mint(address to) external onlyPropertyOwner {
+    _safeMint(msg.sender, tokenId);
     tokenId++;
   }
 
+  function registerProperty(
+    string _propertyOwner,
+    string _propertyAddress,
 
-  function approveLicense() {
+    ) external onlyLicensed {
+
+
+
+  }
+
+
+  function approveProperty() external onlyPropertyOwner {
+
+  }
+
+  function approveLicense() external  {
     // Oracle call here
     if (true) {
       licensed[msg.sender] = true;
