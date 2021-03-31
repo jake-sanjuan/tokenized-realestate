37bbff888b484174e3532b020c72e0e828ae690f
Jacob San Juan <contactjsj42@gmail.com>
2021-03-31 19:20:58 -0400

    Added CL functionality, added events.  Not workable yet

--- contracts/Tokenizer.sol
+++ contracts/Tokenizer.sol
@@ -3,29 +3,19 @@ pragma solidity ^0.7.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-// Need to implememnt IERC721Receiver to receive tokens
-  // Maybe not, do not want this contract to actually handle tokens
-// Need oracle call to verify agent and propertyOwner
 // Need to figure out IPFS storage, needs to be private, Nic sent good links
-  // This will be through front end, pass token URI into contract
+  // This will be through front end
 // Add events
+// Contract needs to be funded with LINK
 
 /*
   Need to figure out how to verify authenticity of agent.  Ideas (Extra to Oracle):
     - Have already approved agent validate data offchain after oracle call,
-      confirm vaidation.  Will involve somehow validating on front end, maybe just
+      confirm validation.  Will involve somehow validating on front end, maybe just
       upload docs to IPFS with no NFT?
-*/
-
-/*
-  Will proabably need to validate transactions that have happened as well.
-  A few ways to do this (All extra to oracle):
-    - Multiple approved real estate agents can validate onchain
-    - Escrow type transaction where both seller and buyer approve transaction
-      proving that it happened
-    - Best idea is likley to have owner validation.  If owner is validated and
-      proved to be a person, there is no problem here
 */
 
 /*
@@ -33,83 +23,142 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
     - Need to upload license, proof of id to IPFS at very least
     - Could this be a seperate NFT? Might have to significantly alter contract,
       tokenized real estate licensing isn't a bad idea though
-*/
-
-/*
-  Real estate stuff (Will need to be mapped to NFT)
     - Need title, certificate of title, deed uploaded to IPFS
     - Also could have a section for 'arbitrary documents'
 */
 
-// Property mint and transfer must be approved by owner
+// Is it unsafe to assign tokenId before minting token?
 
-contract Tokenizer is ERC721 {
+contract Tokenizer is ERC721, ChainlinkClient {
+
+  AggregatorV3Interface internal priceFeed;
 
   mapping (address => bool) public licensed;
   mapping (address => bool) public owner; // Can possible use _owners from ERC721, need to test if mapping to uint will return true
-  mapping (address => mapping (uint => Property)) ownerToIdToProperty;
+  mapping (address => bool) public approved; // Allows users that have sold property to remain validated
+  mapping (address => bool) public newOwner; // Used for new owners that have not changed name yet
+  mapping (address => address) public registeringAgent;
+  mapping (address => uint) public numAgentApprovals;
+  mapping (address => Property) public awaitingApproval;
+  mapping (address => mapping (uint => Property)) ownerToIdToPropertyApproved;
+
 
   struct Property {
     bytes32 propertyOwner; // Hash
     bytes32 propertyAddress; // Hash
     uint currentPrice;
-    bool approved;  // Approved by owner
+    uint changePriceCount;
     bool saleApproved;
+    bool paid;
     uint[] previousPrices;
   }
 
   public string baseURI;  // Not sure if needed, can probably be passed in directly
-  private uint tokenId;
+  public uint tokenId;
+
+  address public oracle;
+  bytes32 public jobId;
+  uint256 public fee;
+
+  /*TODO: Figure out params for events, decide if extra events are needed where
+  events already emitted*/
+  event PropertyRegistered();
+  event AgentApprovedProperty();
+  event SaleApproved();
+  event PaymentRecieved();
+  event PropertyPriceChanged();
+  event OwnerChanged();
+  event LicenseRevoked();
+  event OwnerApproved();
+  event AgentApproved();
 
   constructor() ERC721("Property Token", "PT") {
     tokenId = 0;
+    setPublicChainlinkToken();
+    oracle = // Find oracle
+    jobId = // Figure out where to get jobId
+    fee = // Will depend on oracle used
+    priceFeed = AggregatorV3Interface(/*TODO: Get address for ETH USD*/);
   }
 
   modifier onlyLicensed() {
     require(licensed[msg.sender] == true, "Must be verified to call function.");
+    _;
   }
 
   modifier onlyPropertyOwner() {
     require(owner[msg.sender] == true, "Must be property owner to call function.");
+    _;
   }
 
-  function mint(address to, uint _tokenId) internal {
-    _safeMint(to, _tokenId);
+  modifier licensedOrOwner() {
+    require(
+      licensed[msg.sender] || owner[msg.sender],
+      "Must be either owner or agent to call this function."
+    );
+    _;
   }
 
-  // Map _propertyOwner address to property struct, property struct to tokenId
+  modifier onlyNewOwner() {
+    require(newOwner[msg.sender] == true, "Only new owner can call this function.");
+    _;
+  }
+
+  modifier approvedByAgent(address _toBeApproved) {
+    require(
+      numAgentApprovals[_toBeApproved] > 0,
+      "Must have agent verify before calling function"
+    );
+    _;
+  }
+
+
   function registerProperty (
     string _propertyOwner,
     string _propertyAddress,
     uint salt,  // Will be displayed as pin to user on frontend
     uint _currentPrice,
     address _propertyOwner
-    )
+  )
     external
-    onlyLicensed
-    returns(uint)
+    onlyPropertyOwner
   {
     Property storage property;
 
     property.propertyOwner = keccack256(_propertyOwner + salt);
     property.propertyAddress = keccak256(_propertyAddress + salt);
     property.currentPrice = _currentPrice;
-    property.approved = false;
+    property.paid = false;
     property.saleApproved = false;
 
-    ownerToIdToProperty[_propertyOwner][tokenId] = property;
+    awaitingApproval[msg.sender] = property;
 
-    tokenId++; // This should probably be somewhere else, either higher up or in its own function.  Reentrancy?
+    emit RegisteredProperty();
+  }
+
+  function approveProperty(address _propertyOwner) external onlyLicensed {
+
+    Property storage property = awaitingApproval[_propteryOwner];
+
+    registeringAgent[_propertyOwner] = msg.sender;
+    ownerToIdToPropertyApproved[_propertyOwner][tokenId] = property;
+
+    tokenId++;
+
+    delete(awaitingApproval[_propertyOwner]);
+
+    emit AgentApprovedProperty();
 
     return tokenId - 1;
   }
 
-  function approveAndMintProperty(
+  //Emits "Transfer" event via "mint()" function
+  function mintProperty(
     uint _tokenId,
     string _propertyOwner,
     string _propertyAddress,
     uint salt
-    )
+  )
     external
     onlyPropertyOwner
   {
@@ -120,64 +169,186 @@ contract Tokenizer is ERC721 {
       "Function caller does not own property with that ID"
     );
 
-    property.approved = true;
-
     mint(msg.sender, _tokenId);
   }
 
-  // Might need approval from ERC721? Not sure here
   function approveForSaleByOwner(uint _tokenId) external onlyPropertyOwner {
-    Property storage property = ownerToIdToProperty[msg.sender][_tokenId];
+    Property storage property = ownerToIdToPropertyApproved[msg.sender][_tokenId];
 
     property.saleApproved = true;
+    emit SaleApproved();
   }
 
-  // Which modifier? Need approval from both agent and owner?
-  // Need to make sure owner owns _tokenId, not sure if multiple modifiers or require statement
-    // Can use _owners from ERC721, need to access private variable though
-  // Not sure if ownerToIdToProperty will automatically update here, need to test that
-  function sell(address _to, uint _tokenId) external  {
-    Property storage property = ownerToIdToProperty[msg.sender][_tokenId];
+  // Need functionality to accept payment and route to agent so they can actually sell
+  // Escrow somehow?
+  function payment(uint _tokenId, address owner) external payable onlyNewOwner {
+    Property storage property = ownerToIdToPropertyApproved[owner][_tokenId];
 
-    safeTransfer(msg.sender, _to, _tokenId);
+    require(msg.value == property.currentPrice, "Must send exact amount");
 
-    //changeOwner()
-    //Set Property.saleApproved to false
-    //owner[msg.sender] = false
+    property.paid = true;
+
+    emit PaymentRecieved();
   }
 
-  // Add currentPrice to previousPrices array
-  function changeCurrentPrice() {
+  // Idea is that agent will receive ether and send to owner.  Need to figure out how to stop bad actors
+  // emits Transfer event ERC721
+  function sell(
+    address _from,
+    address _to,
+    uint _tokenId
+  )
+    external
+    onlyLicensed
+  {
+    Property storage property = ownerToIdToPropertyApproved[_from][_tokenId];
+    require(property.saleApproved, "Owner must have approved sale");
+    require(property.paid, "Payment to contract must be received before function call");
 
+    safeTransfer(_from, _to, _tokenId);
+    _from.transfer(property.currentPrice);
+
+    property.saleApproved = false;
+    property.paid = false;
+
+    ownerToPropertyId[_to][_tokenId] = property;
+
+    delete(ownerToIdToPropertyApproved[_from][_tokenId]);
+    newOwner[_to] = true;
+    owner[_from] = false;
   }
 
-  function changeOwner() internal {
+  // Might need to look at functionality here, would make sense to split to two functions
+  // Second would be internal and automatically execute when property.priceChangeCount == 2
+  function changeCurrentPrice(
+    address owner,
+    uint _tokenId,
+    uint newPrice
+  )
+    external
+    licensedOrOwner
+  {
+    Property storage property = ownerToIdToPropertyApproved[owner][_tokenId];
 
+    property.changePriceCount++
+
+    if (property.changePriceCount == 2) {
+      property.previousPrices.push(property.currentPrice);
+      property.currentPrice = newPrice;
+      property.changePriceCount = 0;
+      emit PropertyPriceChanged();
+    }
   }
 
+  function changeOwner(string newOwnerName, uint salt, uint _tokenId) external onlyNewOwner {
+    Property storage property = ownerToIdToPropertyApproved[msg.sender][tokenId];
+    property.propertyOwner = keccak256(newOwnerName + salt);
+    newOwner[msg.sender] = false;
+    owner[msg.sender] = true;
+    emit OwnerChanged();
+  }
+
+  // Emits Transfer event
   function burn(uint _tokenId) external onlyPropertyOwner {
     _burn(tokenId);
   }
 
   function revokeLicense() external onlyLicensed {
-
+    licensed[msg.sender] = false;
+    emit LicenseRevoked();
   }
 
   // Oracle call to validate and approve the property owner.  Must also be approved by agent, not one
   // That registered property
-  function approvePropertyOwner() external {
-    // Oracle call here`
-    if(true) {
+  function approvePropertyOwner(
+    address _potentialOwner
+  )
+    external
+    approvedByAgent(_potentialOwner)
+  {
+    requestOwnerData();
+    if(/*Oracle call works*/) {
       owner[msg.sender] = true;
+      emit OwnerApproved();
     }
   }
 
-// Oracle call will validate and approve property owner.  Must also be approved by other agent
-  function approveLicense() external  {
-    // Oracle call here
-    if (true) {
+
+  // Oracle call will validate and approve property owner.  Must also be approved by other agent
+  function approveLicense(
+    address _potentialAgent
+  )
+    external
+    approvedByAgent(_potentialAgent)
+  {
+
+    requestAgentData();
+    if (/*Oracle call works*/) {
       licensed[msg.sender] = true;
+      emit AgentApproved();
     }
+  }
+
+  function agentApproval(address toBeApproved) external onlyLicensed {
+    require(
+      msg.sender != registeringAgent[toBeApproved],
+      "Agent that registered property cannot also approve owner"
+    );
+
+    numAgentApprovals[toBeApproved]++;
+  }
+
+  //Emits ChainlinkFulfilled event
+  function fulfill(
+    bytes32 _requestId,
+    /*TODO: Add type here*/
+  )
+    external
+    recordChainlinkFulfillment
+  {
+    /*TODO: Return info, bool?*/
+  }
+
+  function homeValueEthToUsd(uint tokenId, address owner) external view returns (uint) {
+    Property memory property = ownerToIdToPropertyApproved[owner][_tokenId];
+
+    (
+        uint80 roundId,
+        int price,
+        uint startedAt,
+        uint timestamp,
+        uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+    return price * property.currentPrice;
+  }
+
+  // Might not be able to be internal
+  // Emits "ChainlinkRequested" event
+  function requestOwnerData() internal returns (bytes32 requestId) {
+
+    Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+
+    request.add("get", /*TODO: Add Path*/);
+    request.add("path", /*TODO: Destructure JSON*/);
+
+    return sendChainlinkRequest(oracle, request, fee);
+  }
+
+  // Might not be able to be internal
+  // Emits "ChainlinkRequested" event
+  function requestAgentData() internal returns (bytes32 requestId) {
+
+    Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+
+    request.add("get", /*TODO: Add Path*/);
+    request.add("path", /*TODO: Destructure JSON*/);
+
+    return sendChainlinkRequest(oracle, request, fee);
+  }
+
+  // Emits ERC721 "Transfer" event
+  function mint(address to, uint _tokenId) internal {
+    _safeMint(to, _tokenId);
   }
 }
 
