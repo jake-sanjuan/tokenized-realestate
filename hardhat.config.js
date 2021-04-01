73325cb63f5d0592f0b12c5393ea8ffaeeb7c3ab
Jacob San Juan <contactjsj42@gmail.com>
2021-04-01 16:46:53 -0400

    Compiled contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,7 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: {
+    version: "0.6.2"
+  }
 };
-
