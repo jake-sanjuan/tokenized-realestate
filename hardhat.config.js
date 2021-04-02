79518b1c6708a42e7b200122d68fd59da3cf1284
Jacob San Juan <contactjsj42@gmail.com>
2021-04-02 10:26:35 -0400

    Config to deploy to Ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,7 +18,22 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {
+
+    },
+    ropsten: {
+      url: process.env.ALCHEMY_API,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
   solidity: {
-    version: "0.6.2"
+    version: "0.6.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   }
 };
